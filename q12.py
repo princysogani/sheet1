b1221ee153c75a42e44f285774975fb26649ec76
princysogani <100213896+princysogani@users.noreply.github.com>
2022-02-26 00:32:47 +0530

    Add files via upload

---
+++ q12.py
@@ -0,0 +1,8 @@
+d = {'Student': ['Rahul', 'Kishore', 'Vidhya','Raakhi'],'Marks': [57,87,67,79]}
+students = d['Student']
+marks = d['Marks']
+maximum = max(marks)
+x= marks.index(maximum)
+print("the student obtained maximum marks is: ", students[x])
+
+    
