0d14c7b0637deb3b58b5ed13312ffb8794b13542
princysogani <100213896+princysogani@users.noreply.github.com>
2022-02-25 23:27:04 +0530

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,31 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+==== No Subprocess ====
+
+WARNING: Running IDLE without a Subprocess is deprecated
+and will be removed in a later version. See Help/IDLE Help
+for details.
+
+>>> 5**9
+1953125
+>>> 3//2
+1
+>>> 7//3
+2
+>>> 7/3
+2.3333333333333335
+>>> 6==6
+True
+>>> a=20; a+=30; a%=3; print(a)
+2
+>>> True * False
+0
+>>> True & False
+False
+>>> ((6>3) and (7<4) or (18==3)) and (9>3)
+False
+>>> True is False
+False
+>>> ((True==False) or (False>True)) and (False<=True)
+False
+>>> 
