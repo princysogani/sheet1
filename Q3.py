9afc48f4c31f8e247d624f37cbf1a9132c1de709
princysogani <100213896+princysogani@users.noreply.github.com>
2022-02-25 23:42:10 +0530

    Add files via upload

---
+++ Q3.py
@@ -0,0 +1,13 @@
+Python 3.9.5 (tags/v3.9.5:0a7dcbd, May  3 2021, 17:27:52) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+==== No Subprocess ====
+
+WARNING: Running IDLE without a Subprocess is deprecated
+and will be removed in a later version. See Help/IDLE Help
+for details.
+
+>>> s1 = "nice to have it "
+>>> s2= "here"
+>>> print(s1 +s2)
+nice to have it here
+>>> 
